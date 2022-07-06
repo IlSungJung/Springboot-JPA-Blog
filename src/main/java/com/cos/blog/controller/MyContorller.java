7457a5b87c41210e431b70b13186bcb4c34083b5
IlSungJung <97210382+IlSungJung@users.noreply.github.com>
2022-07-06 21:04:23 +0900

    Update MyContorller.java

--- src/main/java/com/cos/blog/controller/MyContorller.java
+++ src/main/java/com/cos/blog/controller/MyContorller.java
@@ -3,6 +3,8 @@ package com.cos.blog.controller;
 public class MyContorller {
 
     public void myPrint() {
-        System.out.println("aaa");
+        System.out.println("acccc");
+        
+        System.out.println("11111");
     }
 }
